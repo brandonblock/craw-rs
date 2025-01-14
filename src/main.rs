685d2be99d208ef7b572f654a125a6ed2e8b9a90
Brandon Block <brandon.block@pm.me>
2025-01-14 17:09:07 -0600

    Add ws echo and http health check (#1)
    * dummy ws endpoint
    
    * add health check
    
    * ws echo

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpResponse, HttpServer};
+
+mod handlers;
+
+async fn health_check() -> HttpResponse {
+    // TODO: Actually check for health (db connection, etc)
+    HttpResponse::Ok().body("okeydokey")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    // TODO: get this from env/startup args
+    let bind_address = "127.0.0.1:8080";
+    println!("starting echo server at {}", bind_address);
+
+    HttpServer::new(|| {
+        App::new()
+            .route("/health", web::get().to(health_check))
+            .route("/ws", web::get().to(handlers::ws_handler))
+    })
+    .bind(bind_address)?
+    .run()
+    .await
 }
