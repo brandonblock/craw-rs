2ca12f33a33c7410d106851fc8749935d77644ee
brandonblock <brandon.block@pm.me>
2025-01-23 13:04:21 -0600

    user model

--- Makefile
+++ Makefile
@@ -4,6 +4,7 @@ run: db
 	cargo run
 
 db:
+	export DATABASE_URL="postgres://postgres:password123@localhost/craw"
 	docker run --name craw-db -e POSTGRES_PASSWORD=password123 -e POSTGRES_DB=craw -p 5432:5432 -d postgres:14
 clean:
 	docker stop craw-db
---
+++ src/models/user.rs
@@ -0,0 +1,16 @@
+use serde::{Deserialize, Serialize};
+use uuid::Uuid;
+
+#[derive(Debug, Serialize)]
+pub struct User {
+    pub id: Uuid,
+    pub username: String,
+    #[serde(skip_serializing)]
+    pub password_hash: String,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct RegisterRequest {
+    pub username: String,
+    pub password: String,
+}
